15729f65d4645b2a28aa40d9729b5befffa8bad9
OceanNuclear <annoyingamygdala@gmail.com>
2019-10-13 20:34:20 +0100

    finished writing the embedding functions

--- Embed.py
+++ Embed.py
@@ -1,110 +1,120 @@
-#!/home/ocean/anaconda3/bin/python3
 from numpy import cos, arccos, sin, arctan, tan, pi, sqrt; from numpy import array as ary; import numpy as np; tau = 2*pi
 import pandas as pd
 import pickle
 
-SHUFFLE_SEED = 0
-TRAIN_THRES = 1
+UNLABELLED = -1
 try:
     import os, sys
     os.chdir(sys.argv[1])
+    if len(sys.argv[1:])>0:
+        exec(''.join(sys.argv[1:]))
 except IndexError:
     print("usage:")
-    print("'python Embedding.py <folder_containing_RawData.csv>/'")
+    print("'python "+sys.argv[0]+" <folder_containing_RawData.csv>/'")
     exit()
-
-def shuffle(df, category_col=3):
-    #record the original order of the indices
-    original_indices = df.index
-    original_order = dict([(original_indices[i], i) for i in range(len(df)) ]) #a dictionary to output iloc when loc is inputted.
-
-    #shuffle it once.
-    np.random.seed(SHUFFLE_SEED)
-    indices = df.index.tolist()
-    np.random.shuffle(indices)
-    df_first_shuffle = df.loc[indices]
-
-    #sort them into len(set(df[category_col])) piles.
-    sorted_by_category_then_by_iloc_in_shuffled_order = []
-    for i in list(set(df[category_col])):
-        matching_in_shuffled_list = df_first_shuffle[ df_first_shuffle[category_col]==i ]
-        matching_indices_in_shuffled_order = matching_in_shuffled_list.index.tolist()
-        sorted_by_category_then_by_iloc_in_shuffled_order.append(matching_indices_in_shuffled_order)
-    
-    #Fill up to the TRAIN_THRES, and then fill in the rest.
-    within_threshold = []
-    for short_list in sorted_by_category_then_by_iloc_in_shuffled_order:
-        within_threshold+=short_list[:TRAIN_THRES]    #within training threshold
-    beyond_threshold = []
-    for i in df_first_shuffle.index:
-        if not (i in within_threshold):
-            beyond_threshold.append(i)
-    final_order = within_threshold + beyond_threshold
-
-    #save the conversion required.
-    unscramble = [(i,original_order[final_order[i]]) for i in range(len(sheet))] #a dictionary storing the .iloc index (after, before)
-    with open('unscramble.pkl', 'wb') as f:
-        pickle.dump(dict(unscramble), f)
-    df_final = df.loc[final_order]
-    return df_final
+try:
+    tr_frame = pd.read_csv("train_data.csv", header=None).fillna('')
+    te_frame = pd.read_csv("test_data.csv", header=None).fillna('')
+except FileNotFoundError:
+    print("Please preprocess that directory with 'Embed.py' first.")
+    exit()
 
 def get_unique_words(descriptions):
     allwords = " ".join(list(descriptions))   #join them into a single string
     allwords = allwords.lower().split(" ") #change all words to lower case to minimize duplications
     uniquewords = list(set(allwords))  #turn it into an iterable set
+    uniquewords.sort()
     uniquewords = [ i for i in uniquewords if (i!="" and i.isprintable())] #remove the empty string character from the set
     print("{0} unique words is found and used as the features for learning.".format(len(uniquewords)))
     return uniquewords #a list of all unique words
 
-def get_set_of_categroies(pdseries):
-    list_of_set_of_category = list(set(pdseries))
-    return list_of_set_of_category
-
-def save_as_dict(set_as_list, data="category"):
+def save_inversion_dict(set_as_list, data="category"):
     allcombo_dict = dict([ [ i,set_as_list[i] ] for i in range(len(set_as_list))]) #convert to a dictionary, which can be saved.
-    with open(data+"_conversion.pkl","bw") as f:
+    with open("inversion_"+data+".pkl","bw") as f:
         pickle.dump(allcombo_dict, f)
     return
+
+def save_conversion_dict(set_as_list, data="category"):
+    allcombo_dict = dict([ [set_as_list[i], i]  for i in range(len(set_as_list)) ])
+    with open("conversion_"+data+".pkl","bw") as f:
+        pickle.dump(allcombo_dict, f)
+    return    
+
+def get_set_of_categroies(pdseries):
+    s = set(pdseries)
+    s.discard('')
+    list_of_set_of_category = list(s)
+    list_of_set_of_category.sort()
+    return list_of_set_of_category
 
 def convert2embedding(pdseries, uniquewords_list):
     new_series = {}
     for index,row in pdseries.iteritems():
         row=row.lower()
-        new_series.update({index : [(word in row) for word in uniquewords_list]})
+        #use number of occurance instead of boolean
+        new_series.update({index : [row.count(word) for word in uniquewords_list]})
     return pd.Series(new_series)
 
 def convert2integerrepresentation(pdseries, category_list):
     new_series = {}
     for index,row in pdseries.iteritems():
         #no lower() operation needed
-        integer_requried = category_list.index(row)#find the matching value
+        try:
+            integer_requried = category_list.index(row)#find the matching value
+        except ValueError:
+            integer_requried = UNLABELLED
         new_series.update({index:integer_requried})
     return pd.Series(new_series)
 
-def get_dict(file):
-    with open(file, "rb") as f:
-        return pickle.load(f)
+def convert2onehot(pdseries):
+    s = set(pdseries)
+    s.discard(UNLABELLED)
+    all_categories = list(s) # using the integer representation, stored in 'category_conversion.pkl', as the column names.
+    all_categories.sort()
+    series = list(pdseries)
+    onehot_result = np.zeros((len(series),len(all_categories)))
+    for i in range(len(series)):
+        try:
+            onehot_result[i][all_categories.index(series[i])]+=1
+        except ValueError: 
+            assert series[i]==UNLABELLED, "This entry has not been labelled with a member of the set of accepted categrories; but it isn't labelled with the integer representation -1 to indicate that it was UNLABELLED to begin with either; so something is wrong."
+    onehot_result = pd.DataFrame(onehot_result, index=pdseries.index, columns=all_categories)
+    return onehot_result #return a DataFrame
 
-def convertback2words(pdseries, uniquewords_dict):
-    pass
-
-#read
-sheet = pd.read_csv("RawData.csv", header=None, index_col = 0) #header=['index', 'description', 'class', 'category']
-
-#Shuffle
-if SHUFFLE_SEED!=None:
-    sheet = shuffle(sheet)
+def split2columns(pdseries):
+    assert type(pdseries) == pd.core.series.Series, "only accept a single column (pd.Series) object"
+    new_df = []
+    for vector in pdseries:
+        new_df.append(vector)
+    new_df = pd.DataFrame(new_df, index = pdseries.index, columns = [ i for i in range(len(vector)) ])
+    return new_df #return another DataFrame
 
 #do the embedding conversion
-hybrid_data = sheet[1].fillna('')+" "+ sheet[2].fillna('')#combine column 1 (description) and 2 (class)
+hybrid_data = tr_frame[1]+" "+ tr_frame[2]#combine column 1 (description) and 2 (class)
 uniquewords = get_unique_words(hybrid_data) #combine the "description" with "class" to get the hybrid embedding
-save_as_dict(uniquewords, data="description_and_class")
-list_of_set_of_category = get_set_of_categroies(sheet[3])
-save_as_dict(list_of_set_of_category)
 
+save_inversion_dict(uniquewords, data="description_and_class")
+save_conversion_dict(uniquewords, data="description_and_class")
+list_of_set_of_category = get_set_of_categroies(tr_frame[3])
+save_inversion_dict(list_of_set_of_category)
+save_conversion_dict(list_of_set_of_category)
 
 #save dataframe with only embedding
-embedding_representation = convert2embedding(hybrid_data, uniquewords)
-category_integer_representation = convert2integerrepresentation(sheet[3], list_of_set_of_category)
-preprocessed_data = pd.DataFrame({'feature':embedding_representation,'label':category_integer_representation})
-preprocessed_data.to_csv('preprocessed_data.csv')
+frames = {'tr':tr_frame, 'te':te_frame}
+train_test_feature_label = {}
+for t in ('tr', 'te'):
+    embedding_representation = convert2embedding(hybrid_data, uniquewords)
+    category_integer_representation = convert2integerrepresentation(frames[t][3], list_of_set_of_category)
+    # preprocessed_data = pd.DataFrame({'feature':embedding_representation,'label':category_integer_representation})
+    train_test_feature_label.update({t+'fea':split2columns(embedding_representation)})
+    train_test_feature_label.update({t+'lab':convert2onehot(category_integer_representation)})
+
+#   train_test_feature_label = {'trfea': ,
+                                # 'trlab': ,
+                                # 'tefea': ,
+                                # 'telab': ,
+                                # }
+
+for k,v in train_test_feature_label.items():
+    with open(k+'.pkl', 'wb') as f:
+        pickle.dump(v,f)
